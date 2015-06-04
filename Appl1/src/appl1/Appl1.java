7a16ad13b1dd09a25f7c6571a4484698f922ff57
Admin <Admin@Admin-PC>
2015-06-05 00:40:26 +0200

    Affiche2

--- Appl1/src/appl1/Appl1.java
+++ Appl1/src/appl1/Appl1.java
@@ -14,8 +14,10 @@ public class Appl1 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("ee");
+        System.out.println("zzze");
     }
     
 }
